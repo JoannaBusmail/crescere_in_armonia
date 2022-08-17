42d8be43185978f5d7f1bf7b0c2d408ff32c3c60
JoannaBusmail <joanna.busmail@gmail.com>
2022-08-17 18:03:03 +0200

    add little style in info section

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,7 +2,7 @@ import '../styles/components/infoList.scss';
 
 function Info(props) {
   return (
-    <li>
+    <li className='article'>
       <img src={props.image} alt={props.infoimage} />
       <h2>{props.title}</h2>
       <p className='infoList__content'>{props.content}</p>
